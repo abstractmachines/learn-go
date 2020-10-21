3537025b127f9be79e7db3f16859bbb4951f73d6
abstractmachines <amandafalke@datawire.io>
2020-10-20 17:05:16 -0700

    Explicit type conversions must happen via casting.

--- type-conversions.go
+++ type-conversions.go
@@ -4,22 +4,26 @@ import "fmt"
 import "reflect"
 
 func main() {
-	var one = 1
-	var two = 2
-	var resultInt = one * two
+	var oneInt = 1
+	var twoInt = 2
+	var resultInt = oneInt * twoInt
 	fmt.Println(resultInt)
-	fmt.Println(reflect.TypeOf(resultInt)) // int.
+	fmt.Println("typeOf result Int should be int: ", reflect.TypeOf(resultInt)) // int.
 
 	/*
 		Question: Can I do an implicit type conversion in golang?
 		Answer: Nope, you can't (unless it's an untyped constant, more later).
 	*/
 	// The compiler will catch this code. "mismatched types."
-	//var three = 3.0 // float
+	//var threeFloat = 3.0 // float
 	//var resultMultiPass = three * one
 	//fmt.Println(resultMultiPass)
 
-	/* So in golang you have to have an explicit type conversion.
+	/* So in golang you have to CAST for an explicit type conversion.
 	 */
+	fmt.Println(reflect.TypeOf(oneInt))
+	var threeFloat = 3.0 // could also be: var threeFloat float64 = 3.0 ... or threeFloat := 3.0
+	resultFloat := float64(oneInt) * threeFloat
+	fmt.Println(resultFloat)
 	
 }
