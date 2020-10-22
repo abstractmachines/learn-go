288f499c7bee3a74c52d949f921935057ecb0803
abstractmachines <amandafalke@datawire.io>
2020-10-21 17:10:10 -0700

    Slice API methods len() cap() ...

--- slices-and-iteration.go
+++ slices-and-iteration.go
@@ -6,18 +6,19 @@ import "fmt"
 Arrays are limiting because the SIZE is part of the TYPE.
 Arrays really aren't common in golang.
 
-We iterate over the array here with a simple for loop.
+YOU HAVE TO size these arrays in these args or there's a panic runtime error "slice bounds out of range [:3] with capacity 0
+^ I bet I'm missing something about dynamic arrays, but ... remember, Go uses mostly slices, not arrays.
  */
-func anArray() [5]int {
+func createIntArray() [5]int {
 	var intArray [5]int
 
 	var val int = 0
 	for i := 0; i < len(intArray); i++ {
 		intArray[i] = val
-		fmt.Println(intArray[i])
 		val++
 	}
 
+	fmt.Printf("\nthe array is %v", intArray)
 	return intArray
 }
 
@@ -27,7 +28,7 @@ func anArray() [5]int {
 - When ranging over a slice, two values are returned for each iteration.
 The first is the index, and the second is a copy of the element at that index.
  */
-func sliceIt(arrayInt [5]int) {
+func sliceStruct(arrayInt [5]int) {
 	type sliceHeader struct {
 		Length int
 		Index *int
@@ -37,13 +38,28 @@ func sliceIt(arrayInt [5]int) {
 		Length: len(arrayInt),
 		Index: &arrayInt[1], // shows just a memory location. ok, broken. TODO fix it.
 	}
-	//fmt.Println("this is arrayInt in sliceIt before structy tymes: ", arrayInt)
 	fmt.Println("Hey, so we sliced your array within a function at first index: ", aSlice)
 }
 
-func main() {
-	var intArray [5]int = anArray()
-	fmt.Println("this is the int array from main. ", intArray)
+func basicSlice(arr [5]int) []int {
+	var slice1 []int = arr[1:3] // setting a capacity cap() of elements 1->5, so cap() === 4.
+	return slice1
+}
 
-	sliceIt(intArray)
+func main() {
+	var intArray [5]int = createIntArray()
+
+	/*
+	Let's perform a basic slice, and get accustomed to the slice API / methods. len() cap() etc
+	 */
+	var slice1 = basicSlice(intArray)
+	// len() : length of a slice
+	fmt.Printf("\nslice of that array is %v with a length of %v\n", slice1, len(slice1)) // [1, 2] with a length of 2
+	// cap() : capacity of a slice
+	fmt.Println("Capacity of our slice: ", cap(slice1)) // 4
+	
+	// "Reslice our slice and extend it:"
+	// let's use the capacity cap() of a slice to extend our slice to the full length of the array (AFTER our index into array).
+	var sliceReslice = slice1[:cap(slice1)]
+	fmt.Println("Reslice the slice, using the CAPACITY of original slice: ", sliceReslice) // [0, 1, 2, 3, 4]
 }
