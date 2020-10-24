3c2a5feb23194831c115f0f45ab1becafe58913f
abstractmachines <abstractmachines@gmail.com>
2020-10-24 15:53:43 -0700

    Structs: Field tags.

--- struct-reflection.go
+++ struct-reflection.go
@@ -6,6 +6,14 @@ import (
 	"reflect"
 )
 
+/* Structs and Field Tags
+
+Like C, Go has structs (with extra bonuses such as optional Field Tags following the field name and data type).
+
+The Field Tag of a struct is usually a key-value pair that communicates meta information about the field.
+
+The default value for a struct's field tag is an empty string (if not explicitly declared).
+ */
 type Structy struct {
 	AField string `max: "99"`
 	AnotherField int `optional: "yes"`
